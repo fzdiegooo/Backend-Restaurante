1df131ac32b40355b0ae98bb45e7c6d74b7b98bf
Diego <zxiteft21@gmail.com>
2024-09-06 16:23:45 -0500

    First Commit

---
+++ src/main/java/com/example/backendrestaurante/BackendRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRestauranteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendrestaurante/BackendRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
